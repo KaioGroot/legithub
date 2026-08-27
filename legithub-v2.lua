0fc8d7436fb27e043cbacd44624056c4013a5a67
Kaio <kaiomagalhaesxp@gmail.com>
2026-08-26 21:07:06 -0400

    fix: subtle 35% dark overlay on background image

--- legithub-v2.lua
+++ legithub-v2.lua
@@ -266,8 +266,8 @@ Corner(bgImage, 16)
 local bgOverlay = Create("Frame", {
 	Name = "BGOverlay",
 	Size = UDim2.fromScale(1, 1),
-	BackgroundColor3 = Theme.Background,
-	BackgroundTransparency = 1,
+	BackgroundColor3 = Color3.new(0, 0, 0),
+	BackgroundTransparency = 0.35,
 	BorderSizePixel = 0,
 	ZIndex = 1,
 }, root)
